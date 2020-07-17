4af4d57600cc4348edb9e496fae1fcb117bceb62
Dan Blah <dan@blahs.life>
2020-07-17 11:16:33 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 getQuote();
 
 function getQuote() {
-    fetch('https://api.kanye.rest')
+    fetch('https://store.zapier.com/api/records?secret=5APfNFe3DaUs6PHX')
         .then((resp) => resp.json())
         .then(function (data) {
             document.getElementById("quote").innerHTML = data.quote;
