f5d0282a4516905cf8ff4504205a2edc90601ee1
Dan Blah <dan@blahs.life>
2020-07-17 14:30:11 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,5 +6,7 @@ function getQuote() {
         .then(function (data) {
             document.getElementById("status").innerHTML = data.status;
             document.getElementById("last_status").innerHTML = data.statuses.list[1];
+            document.getElementById("last_status2").innerHTML = data.statuses.list[2];
+            document.getElementById("last_status3").innerHTML = data.statuses.list[3];
         });
 }
