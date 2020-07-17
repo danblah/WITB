ba94dc82c284926f6c166a607a078db3dc4c60bf
Dan Blah <dan@blahs.life>
2020-07-17 14:22:05 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,5 @@ function getQuote() {
         .then(function (data) {
             document.getElementById("status").innerHTML = data.status;
             document.getElementById("last_status1").innerHTML = data.statuses.list[1];
-            document.getElementById("last_status2").innerHTML = data.statuses.list[2];
-            document.getElementById("last_status3").innerHTML = data.statuses.list[3];
         });
 }
