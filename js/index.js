b18ac661d887bc034664fbf22e32641bc73903f6
Dan Blah <dan@blahs.life>
2020-07-17 11:22:40 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,6 +4,6 @@ function getQuote() {
     fetch('https://store.zapier.com/api/records?secret=5APfNFe3DaUs6PHX')
         .then((resp) => resp.json())
         .then(function (data) {
-            document.getElementById("status").innerHTML = data.quote;
+            document.getElementById("status").innerHTML = data.status;
         });
 }
